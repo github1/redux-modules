fcf061d4873d489728e1e46591c232e46d11dfe6
github1 <john.collier4@gmail.com>
2021-10-05 01:19:03 +0000

    export StoreFromOptions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { isAction } from './is-action';
 export * from './redux-module';
 export * from './redux-module-store';
+export { StoreFromOptions } from './store-from-options';
 export { createModule } from './impl/redux-module-impl';
